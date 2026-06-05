a77428e40a9714202f574bf9d1e6131b53b4a8c0
Adrain <adrain@users.noreply.github.com>
2026-06-05 22:15:28 +0800

    Update SaveLah app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'savelah-v4';
+const CACHE_VERSION = 'savelah-v5';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const APP_SHELL = [
